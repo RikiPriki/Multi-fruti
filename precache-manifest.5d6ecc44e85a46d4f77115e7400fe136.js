e6b9a038e01840a15f35b467f6a3804d1c1ad802
RikiPriki <n-ivashin@list.ru>
2021-05-29 15:19:09 +0300

    Updates

---
+++ precache-manifest.5d6ecc44e85a46d4f77115e7400fe136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09128c71146f9aa609948f8b4c4c716",
+    "url": "/Multi-fruti/index.html"
+  },
+  {
+    "revision": "fda8f1631a1479b13647",
+    "url": "/Multi-fruti/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "488d4b34685ca7c332bd",
+    "url": "/Multi-fruti/static/css/main.7a1116c8.chunk.css"
+  },
+  {
+    "revision": "fda8f1631a1479b13647",
+    "url": "/Multi-fruti/static/js/2.aa4ccd23.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Multi-fruti/static/js/2.aa4ccd23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488d4b34685ca7c332bd",
+    "url": "/Multi-fruti/static/js/main.6a649f15.chunk.js"
+  },
+  {
+    "revision": "fdc53679523fc98e27df",
+    "url": "/Multi-fruti/static/js/runtime-main.353659a3.js"
+  }
+]);
